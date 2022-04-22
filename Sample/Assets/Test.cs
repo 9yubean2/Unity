8770a3b6f0beac9b0302b74f485584f306108066
9yubean2 <mi701170@naver.com>
2022-04-22 23:19:47 +0900

    C# Script Example

--- Sample/Assets/Test.cs
+++ Sample/Assets/Test.cs
@@ -2,17 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//player class
+public class Player
+{
+    private int hp = 100;
+    private int power = 50;
+
+    public void Attack()
+    {
+        Debug.Log(this.power + "Hit Damage");
+        Debug.Log("Now HP: " + this.hp);
+    }
+
+    public void Damage(int damage)
+    {
+        this.hp -= damage;
+        Debug.Log(damage + "Get Damage");
+        Debug.Log("Now HP: " + this.hp);
+    }
+
+}
+
+
+
 public class Test : MonoBehaviour
 {
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+       Vector2 startPos = new Vector2(3.0f, 4.0f);
+       Vector2 endPos = new Vector2(19.0f, 7.0f);
+       Vector2 dir = endPos - startPos;
+
+       Debug.Log(dir);
+
+       float len = dir.magnitude;
+       Debug.Log(len);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Move object in every frame
     }
 }
